858a857c100bd327c3c77dcd6cb14c3029a784c1
tkjoo <groovejumat@naver.com>
2021-06-10 22:25:32 +0900

    20210610 coding test

---
+++ python/network_count.py
@@ -0,0 +1,25 @@
+def solution(n, computers):
+    answer = 0
+
+    network = []
+    networking = []
+    
+    for i in range(n) :
+        # print(i)
+        visited = []
+        network += [dfs(i, visited, computers)]
+        # print(network)
+    
+    # network = dfs(1, visited, computers)
+    dup = list(set(map(tuple, network)))
+    # print(dup)
+    return len(dup)
+
+
+def dfs(start, visited, computers) :
+    visited += [start]
+    for c in range(len(computers[start])) :
+        if computers[start][c] == 1 and (c not in visited) :
+            dfs(c, visited, computers)
+    visited.sort()
+    return visited
