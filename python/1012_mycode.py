8bd0ecd05bdae2c4bb59ead5c92bc5bf0d8a8516
tkjoo <groovejumat@naver.com>
2021-06-22 07:15:42 +0900

    20210621 coding test

--- python/1012_mycode.py
+++ python/1012_mycode.py
@@ -13,16 +13,17 @@ dy = [1,-1,  0, 0]
 def bfs(nx, ny, m, n, matrix) :
     queue = []
     queue.append([nx, ny])
-    print("넣은 큐값", queue[0])
+    # print("넣은 큐값", queue[0])
     while len(queue) > 0 :
         x, y = queue.pop()
+        
         for i in range(len(dx)) :
-            nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx and nx < m and 0 <= ny and ny < n :
-                if matrix[nx][ny] == 1 :
-                    print("조건에 만족함")
-                    matrix[x][y] = 0
-                    queue.append([nx, ny])
+            nnx, nny = x + dx[i], y + dy[i]
+            if 0 <= nnx and nnx < n and 0 <= nny and nny < m :
+                if matrix[nnx][nny] == 1 :
+                    matrix[nnx][nny] = 0
+                    # print(nnx, nny ,"조건에 만족함")
+                    queue.append([nnx, nny])
              
 
 
@@ -32,14 +33,22 @@ for i in range(testCaseCount) :
     for i in range(k) :
         x, y = map(int, stdin.readline().split())
         matrix[y][x] = 1
-    print(matrix)
+
+
+    # for i in range(len(matrix)) :
+        # print(matrix[i])
     
     cnt = 0
     for i in range(len(matrix)) :
         for u in range(len(matrix[0])) :
             if matrix[i][u] == 1 :
+                # print("//탐색 시작//")
+                # print(m, n)
                 bfs(i, u, m, n, matrix)
                 cnt += 1
     # print(matrix)
     answer.append(cnt)
-    print("정닶값은?" , answer[0])
+
+
+for i in range(len(answer)) :
+    print(answer[i])
